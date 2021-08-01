2b1cdef30d2a58dddd4bcbc0401aa4930da5e112
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-08-01 18:58:47 +0700

    Aggregation Pipeline: Matching and Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,54 +11,6 @@ exports.aliasTopTour = (req, res, next) => {
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
-    // BUILD QUERY
-    // 1.a) Filtering
-    // const queryObj = { ...req.query };
-    // const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    // excludeFields.forEach(el => delete queryObj[el]);
-
-    // // 1.b) Advanced Filtering
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-
-    // { difficulty : "easy", duration: {$gte: 5}}
-    // { difficulty : "easy", duration: {gte: '5'}}
-    // gte, gt, lte, lt
-
-    // let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   // console.log(sortBy);
-    //   query = query.sort(sortBy);
-    //   // sort('price ratingsAverage')
-    // } else {
-    //   query.sort('-createdAt');
-    // }
-
-    // 3) Field Limiting
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    //   // query = query.select('name duration price')
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    // 4) Pagination
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-    // //page=3&limit=10, 1-10, page 1, 11-20, page 2, 21-30, page 3
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) throw new Error('Page does not exist');
-    // }
-
     // EXECUTE QUERY
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
@@ -66,13 +18,6 @@ exports.getAllTours = async (req, res) => {
       .sort()
       .limitFields();
     const tours = await features.query;
-    // query.sort().select().skip().limit()
-
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
 
     // SEND RESPONSE
     res.status(200).json({
@@ -158,6 +103,46 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          // _id: '$ratingsAverage',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+      // {
+      //   $match: { _id: { $ne: 'EASY' } }
+      // }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
     });
   } catch (err) {
     res.status(404).json({
