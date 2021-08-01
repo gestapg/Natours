67f80694bc51d62a900e1010173f12470a961d40
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-08-01 21:24:13 +0700

    Query  Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -72,13 +76,28 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', function(next) {
-  console.log('Will save Document....');
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save Document....');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// Query Middleware
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
 
-tourSchema.post('save', function(doc, next) {
-  console.log(doc);
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took: ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
   next();
 });
 
