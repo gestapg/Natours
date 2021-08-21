9901229ec2586c76a74736dfaceae5c5b8c2bfb2
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-08-22 01:04:59 +0700

    Geospatial Aggregation: Calculating Distances

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -40,6 +40,8 @@ router
 // /tours-distance?distance=233&center=-40,45&unit=miles
 // /tours-distance/233/center/-40,45/unit/miles
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
