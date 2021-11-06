eabd9c4cfa98965375d2ec4816ac03e7dfbf4126
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-11-06 15:37:21 +0700

    Building the tour overview part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get toyr data from collection
+  const tours = await Tour.find();
+
+  // 2) Build templates
+  // 3) Render that temolate using tour data
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
+
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour'
