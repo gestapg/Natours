512606ca7a8671d8a8a6858634222b8caeeed5d3
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-11-06 21:40:37 +0700

    Logging in user with API part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -37,7 +37,13 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account'
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('login', {
+      title: 'Log into your account'
+    });
 };
